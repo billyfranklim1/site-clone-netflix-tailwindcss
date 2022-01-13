ba7e57c31c0fd769f14b8c2f5d82ae85ad8ee179
billyfranklim1 <billyfranklim@gmail.com>
2022-01-13 03:12:47 -0300

    primeira sessao site

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern':
+          "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 50%, rgba(0,0,0,1) 100%)",
+      },
+    },
+  },
+  plugins: [],
+}
