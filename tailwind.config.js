712b69db11d2181eaf1986ea05368498ac0781f2
billyfranklim1 <billyfranklim@gmail.com>
2022-01-20 20:05:53 -0300

    reponsividade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'netflix-border': '#222',
+        'netflix': '#e50914',
+      },
       backgroundImage: {
         'hero-pattern':
           "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 50%, rgba(0,0,0,1) 100%)",
